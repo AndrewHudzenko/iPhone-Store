4a168276e2be87399881e7fa45da8c7bc5a81b26
buuufy <s1mpleworkpost@gmail.com>
2022-11-26 10:46:01 +0200

    start tests

--- src/main/java/com/app/phoneshop/model/Product.java
+++ src/main/java/com/app/phoneshop/model/Product.java
@@ -1,6 +1,9 @@
 package com.app.phoneshop.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,6 +12,8 @@ import javax.persistence.Table;
 import java.math.BigDecimal;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "products")
 public class Product {
